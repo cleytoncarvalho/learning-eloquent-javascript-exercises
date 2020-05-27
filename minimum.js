b238c4ec4900a7092e98fdb81e09607981b78b7d
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2020-05-27 18:43:13 -0300

    Minimum

---
+++ minimum.js
@@ -0,0 +1,9 @@
+/*
+Write a function min that takes two arguments and returns their minimum.
+*/
+
+function minimum(number1, number2) {
+  return number1 < number2 ? number1 : number2;
+}
+
+console.log(minimum(0, -10));
