8edd39f98baa7acdf9b9d2389e879d7aafe5b5f3
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2020-05-27 22:24:09 -0300

    Is even?

---
+++ is-even.js
@@ -0,0 +1,21 @@
+/*
+Define a recursive function isEven corresponding to this description.
+The function should accept a single parameter (a whole number) and return a
+Boolean.
+
+Test it on 50 and 75.
+*/
+
+function isEven(number) {
+  if (!Number.isInteger(number)) {
+    return false;
+  }
+
+  return number % 2 === 0 ? true : false;
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-50));
+console.log(isEven(-75));
+console.log(isEven(0));
